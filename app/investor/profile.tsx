fa2fe7be0f8d92aede2a60e9f8ccedebffa23b03
ShabeenaNaguleswaran <naguleswaranshabeena@gmail.com>
2026-03-13 19:52:15 +0530

    Add imports for investor profile screen

---
+++ app/investor/profile.tsx
@@ -0,0 +1,12 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import {
+  Alert,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
