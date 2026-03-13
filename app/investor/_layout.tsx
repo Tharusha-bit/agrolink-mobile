2616aeaca60ced2194853d7213fe4b2fb3901221
ShabeenaNaguleswaran <naguleswaranshabeena@gmail.com>
2026-03-13 11:43:05 +0530

    Add imports and module dependencies

--- app/investor/_layout.tsx
+++ app/investor/_layout.tsx
@@ -1,54 +1,63 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
-import { ProjectProvider } from '../src/context/ProjectContext'; 
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { ProjectProvider } from "../src/context/ProjectContext";
 
 export default function RootLayout() {
   return (
-  <ProjectProvider>
-    <Tabs
-      screenOptions={{
-        headerShown: true, 
-      }}
-    >
-      <Tabs.Screen
-        name="home"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" size={28} color={color} />
-          ),
+    <ProjectProvider>
+      <Tabs
+        screenOptions={{
+          headerShown: true,
         }}
-      />
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" size={28} color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="invest"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="storefront" size={28} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="invest"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="storefront"
+                size={28}
+                color={color}
+              />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="dashboard"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="chart-box-outline" size={28} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="dashboard"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="chart-box-outline"
+                size={28}
+                color={color}
+              />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account-circle-outline" size={28} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  </ProjectProvider>
-
-  
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="account-circle-outline"
+                size={28}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </ProjectProvider>
   );
 }
-
