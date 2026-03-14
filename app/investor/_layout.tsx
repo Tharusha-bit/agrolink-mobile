86df5d17c9f8e64d8674f0074099d6d0f6aec80d
ShabeenaNaguleswaran <naguleswaranshabeena@gmail.com>
2026-03-14 10:34:34 +0530

    fix: correct ProjectContext import path in investor layout

--- app/investor/_layout.tsx
+++ app/investor/_layout.tsx
@@ -1,6 +1,6 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-import { ProjectProvider } from "../src/context/ProjectContext";
+import { ProjectProvider } from "../../src/context/ProjectContext";
 
 export default function RootLayout() {
   return (
